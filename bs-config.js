c483e2cec933e9ca22f070452edd5a04bec7f57c
Yash Ghelani <yashnghelani@icloud.com>
2017-09-16 16:32:36 +0530

    Add ES Lint support, BrowserSync support and move all files to the public folder

---
+++ bs-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"ui": false,
+	"files": "public",
+	"watchEvents": [
+		"change"
+	],
+	"watchOptions": {
+		"ignoreInitial": true
+	},
+	"server": "public",
+	"port": 2222,
+	"https" : true,
+	"logLevel": "silent",
+	"logPrefix": "Need",
+	"logFileChanges": false,
+	"logSnippet": false,
+	"notify": false,
+	"minify": true
+};
